3118570b9a78b0fa1955da99f1843f6790072e53
mcarn <mlcarnahan22@gmail.com>
2019-02-25 14:35:40 -0600

    Added activities

--- app/src/main/java/edu/apsu/simon/MainActivity.java
+++ app/src/main/java/edu/apsu/simon/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.apsu.simon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        Button b = findViewById(R.id.elimination_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EliminationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b = findViewById(R.id.rewind_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RewindActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b = findViewById(R.id.surprise_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SurpriseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        b = findViewById(R.id.swipe_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SwipeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/edu/apsu/simon/RewindActivity.java
@@ -0,0 +1,14 @@
+package edu.apsu.simon;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class RewindActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewind);
+    }
+}
