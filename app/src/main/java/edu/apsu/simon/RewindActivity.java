d37292212317e7630e145587fb42bc00799c04e0
mcarn <mlcarnahan22@gmail.com>
2019-03-11 18:29:12 -0500

    Added Sound

--- app/src/main/java/edu/apsu/simon/RewindActivity.java
+++ app/src/main/java/edu/apsu/simon/RewindActivity.java
@@ -1,17 +1,25 @@
 package edu.apsu.simon;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class RewindActivity extends AppCompatActivity {
     private ArrayList<Integer> sequence;
@@ -21,8 +29,13 @@ public class RewindActivity extends AppCompatActivity {
     private boolean greenClicked;
     private boolean yellowClicked;
     private boolean redClicked;
-
-
+    private Handler handler;
+    private SoundPool soundPool;
+    private Set<Integer> soundsLoaded;
+    int blueSampleId;
+    int greenSampleId;
+    int yellowSampleId;
+    int redSampleId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +44,36 @@ public class RewindActivity extends AppCompatActivity {
 
         //creates a new array list sequence
         sequence = new ArrayList<>();
+
+        handler = new Handler();
+
+        soundsLoaded = new HashSet<Integer>();
+
+        AudioAttributes.Builder atrBuilder = new AudioAttributes.Builder();
+        atrBuilder.setUsage(AudioAttributes.USAGE_GAME);
+
+        SoundPool.Builder spBuilder = new SoundPool.Builder();
+        spBuilder.setAudioAttributes(atrBuilder.build());
+        spBuilder.setMaxStreams(1);
+
+        soundPool = spBuilder.build();
+
+        soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                if(status == 0){
+                    soundsLoaded.add(sampleId);
+                }
+                else{
+                    Log.i("SOUND", "Error, cannot load sound.  Status = " + status);
+                }
+            }
+        });
+
+        blueSampleId = soundPool.load(this, R.raw.blue, 1);
+        greenSampleId = soundPool.load(this, R.raw.green, 1);
+        yellowSampleId = soundPool.load(this, R.raw.yellow, 1);
+        redSampleId = soundPool.load(this, R.raw.red, 1);
 
         Button b = findViewById(R.id.start_button);
         b.setOnClickListener(new StartListener());
@@ -53,8 +96,6 @@ public class RewindActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             createSequence();
-
-            checkSequence();
         }
     }
 
@@ -62,6 +103,7 @@ public class RewindActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             blueClicked = true;
+            playSound(blueSampleId);
         }
     }
 
@@ -69,6 +111,7 @@ public class RewindActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             greenClicked = true;
+            playSound(greenSampleId);
         }
     }
 
@@ -76,6 +119,7 @@ public class RewindActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             yellowClicked = true;
+            playSound(yellowSampleId);
         }
     }
 
@@ -83,33 +127,51 @@ public class RewindActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             redClicked = true;
+            playSound(redSampleId);
         }
     }
+
+
 
     private void createSequence(){
         random = new Random();
         randomButton = random.nextInt(4)+1;
         sequence.add(randomButton);
+        sequence();
+
     }
 
-    private void checkSequence(){
+    private void sequence(){
         //uses a for loop to check the sequence in reverse
         for(int i = sequence.size() - 1; i >= 0; i--){
             int seq = sequence.get(i);
 
-            if(seq == 1 && blueClicked){
+            if(seq == 1){
 
-            } else if(seq == 2 && greenClicked){
-
-            } else if(seq == 3 && yellowClicked){
-
-            } else if(seq == 4 && redClicked){
-
-            } else {
-                gameOver();
+                findViewById(R.id.blue_imageButton).setBackgroundColor(Color.BLUE);
+            }
+            else if(seq == 2){
+                findViewById(R.id.green_imageButton).setBackgroundColor(Color.GREEN);
+            }
+            else if(seq == 3){
+                findViewById(R.id.red_imageButton).setBackgroundColor(Color.RED);
+            }
+            else if(seq == 4){
+                findViewById(R.id.yellow_imageButton).setBackgroundColor(Color.YELLOW);
             }
 
             Log.i("SEQUENCE", "i is " + i + " sequence is " + seq);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void playSound(int sampleId){
+        if(soundsLoaded.contains(sampleId)){
+            soundPool.play(sampleId, 1.0f, 1.0f, 0, 0, 1.0f);
         }
     }
 
